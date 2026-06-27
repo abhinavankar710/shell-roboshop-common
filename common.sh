b176ae7be2d816d678daa9f55775fa2e62715b70
abhinavankar710 <abhinav.ankar710@gmail.com>
2026-06-27 17:50:43 +0530

    Roboshop

---
+++ common.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+check_root(){
+    USERID=$(id -u)
+        if [ $USERID -ne 0 ]; then
+            echo "ERROR: Please run this script with root privileges"
+            exit 1
+        fi
+}
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+SCRIPT_DIR=$PWD
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+mkdir -p $LOGS_FOLDER
+START_TIME=$(date +%s)
+echo "Script execution started at: $(date)" | tee -a $LOG_FILE
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+spinner() {
+    local pid=$1
+    local text=$2
+    local delay=0.1
+    local spinstr='▏▎▍▌▋▊▉█▉▊▋▌▍▎'
+    local i=0
+    tput civvis 
+    while kill -0 "$pid" 2>/dev/null; do
+        printf "\r%b...%c" "$text" "${spinstr:$i:1}"
+        i=$(( (i + 1) % ${#spinstr} ))
+        sleep $delay
+    done
+    printf "\r\033[K"
+    tput cnorm 
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2...$R✗ Failed$N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2...$G✓ Success$N" | tee -a $LOG_FILE
+    fi
+}
+
+
+print_total_time(){
+    END_TIME=$(date +%s)
+    TOTAL_TIME=$(( END_TIME - START_TIME ))
+    echo "Script execution completed at: $(date)" | tee -a $LOG_FILE
+    echo "Total time taken: $TOTAL_TIME seconds" | tee -a $LOG_FILE
+}
---
+++ mongodb.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+source ./common.sh
+
+check_root
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
+VALIDATE $? "MongoDB Repository Setup"
+
+dnf list installed mongodb-org &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+    echo -ne "${Y}Installing${N} MongoDB"
+    
+    # --- THE FIX STARTS HERE ---
+    # We run the install AND save the exit code to a file inside this block ( )
+    (
+        dnf install -y mongodb-org &>>$LOG_FILE
+        echo $? > /tmp/mongo_status
+    ) & 
+    
+    pid=$!
+    spinner $pid
+    wait $pid
+    
+    # We read the code from the file so it is NEVER empty
+    EXIT_STATUS=$(cat /tmp/mongo_status)
+    
+    # We validate using that solid number
+    VALIDATE $EXIT_STATUS "MongoDB Installation"
+    # --- THE FIX ENDS HERE ---
+    
+else
+    echo -e "MongoDB already exists$Y SKIPPING$N installation of MongoDB" | tee -a $LOG_FILE
+fi
+
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "Enabling MongoDB Service"
+
+systemctl start mongod &>>$LOG_FILE
+VALIDATE $? "Executing MongoDB Service"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing Remote Connections to MongoDB"
+
+systemctl restart mongod &>>$LOG_FILE
+VALIDATE $? "Restarting MongoDB Service"
+
+print_total_time
